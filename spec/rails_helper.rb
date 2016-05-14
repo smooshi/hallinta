99ae8a8d55815d349182b1f4b9438c1615fc5750
smooshi <timereh@gmail.com>
2016-05-14 16:10:52 +0300

    ?

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,18 @@
 require 'simplecov'
-SimpleCov.start('rails')
+SimpleCov.start 'rails' do
+  require 'simplecov-badge'
+  # add your normal SimpleCov configs
+  add_filter "/app/admin/"
+  # configure any options you want for SimpleCov::Formatter::BadgeFormatter
+  SimpleCov::Formatter::BadgeFormatter.generate_groups = true
+  SimpleCov::Formatter::BadgeFormatter.strength_foreground = true
+  SimpleCov::Formatter::BadgeFormatter.timestamp = true
+  # call SimpleCov::Formatter::BadgeFormatter after the normal HTMLFormatter
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+      SimpleCov::Formatter::HTMLFormatter,
+      SimpleCov::Formatter::BadgeFormatter,
+  ]
+end
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
