559eba52f06414b8f3a68ede480d3572cc99217f
smooshi <timereh@gmail.com>
2016-05-14 16:33:53 +0300

    ?

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Role.create(name: "ADMIN", description: "Admining")
+User.create(first_name:"Pekka", last_name:"Pouta", admin: true, email: "email@uuh.com", role_id: 1)
+CompanyType.create(name: "Komppis", description:"Kamppis")
+Company.create(name:"Komppani", email:"eh@eh.com", company_type_id: 1)
+Restaurant.create(name:"Ravinteli", company_id: 1, email:"ehhe@hoh.com")
