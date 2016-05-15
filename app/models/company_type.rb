8a364973bbd5893c1243a68b48f20db2f16f64e4
smooshi <timereh@gmail.com>
2016-05-15 11:43:46 +0300

    hih

--- app/models/company_type.rb
+++ app/models/company_type.rb
@@ -1,4 +1,4 @@
 class CompanyType < ActiveRecord::Base
   has_many :companies, :dependent => :restrict_with_exception
-  validates :name, :presence => true, :message => "Names are mandatory."
+  validates :name, :presence => true
 end
